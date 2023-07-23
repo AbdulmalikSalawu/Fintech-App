89f868530559ecf4d427941ff858bf17e9cf15fc
Salawu Abdulmalik <salawuabdulmalik90@gmail.com>
2023-07-23 00:26:03 -0700

    validated the sign-up form with formik and regex toolkits

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -5,6 +5,8 @@ import '../Styles/Signup.css'
 import whiteLogo from '../Assets/whiteLogo.svg';
 import { navNotNeeded } from "../Features/navSlice";
 import { useNavigate } from "react-router";
+import {useFormik} from 'formik';
+import {basicSchema} from '../Schemas/schema';
 
 function Signup() {
   const [firstname,setFirstname] = useState("")
@@ -18,7 +20,9 @@ function Signup() {
 
   useEffect(() => {
     dispatch(navNotNeeded())
-  }, []) 
+  }, [])
+  
+  
 
   const registerUser = ()=>{
     const url = "http://localhost:5000/signup"
@@ -33,6 +37,18 @@ function Signup() {
     })
   }
 
+  const {values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit} = useFormik({
+    initialValues: {
+        username: "",
+        lastname: "",
+        email: "",
+        phoneNumber: "",
+        password: ""
+    },
+    validationSchema: basicSchema,
+    registerUser,
+})
+
   return (
     <div className="pt-3 body">
       {showNav ? (<div>
@@ -40,11 +56,22 @@ function Signup() {
         <h4 className="text-center text-success mt-4">{message}</h4>
         <div class='inputCont col-md-8 col-lg-4 d-block m-auto'>
         <h5 class='text-center fw-bold mt-3 px-5 fs-2'>Create an account with us today</h5 >
-          <input type="text" placeholder="firstname" name="firstname" className="form-control w-75 d-block m-auto mt-5 text-center py-2" onChange={(e)=>(setFirstname(e.target.value))} />
-          <input type="text" placeholder="lastname" name="lastname" className="form-control w-75 d-block m-auto mt-4 text-center py-2" onChange={(e)=>(setLastname(e.target.value))} />
-          <input type="text" placeholder="email" name="email" className="form-control w-75 d-block m-auto mt-4 text-center py-2" onChange={(e)=>(setEmail(e.target.value))} />
-          <input type="text" placeholder="phone-number" name="phone-number" className="form-control w-75 d-block m-auto mt-4 text-center py-2" onChange={(e)=>(setEmail(e.target.value))} />
-          <input type="password" placeholder="password" name="password" className="form-control w-75 d-block m-auto mt-4 text-center py-2" onChange={(e)=>(setPassword(e.target.value))} />
+
+          <input type="text" placeholder="firstname" className="form-control w-75 d-block m-auto mt-5 text-center py-2" value={values.username}  onChange={handleChange} onBlur={handleBlur} name='username' />
+          {errors.username && touched.username && <p className='error text-center'>{errors.username}</p>}
+
+          <input type="text" placeholder="lastname" name="lastname" className="form-control w-75 d-block m-auto mt-4 text-center py-2" value={values.lastname}  onChange={handleChange} onBlur={handleBlur} />
+          {errors.lastname && touched.lastname && <p className='error text-center'>{errors.lastname}</p>}
+
+          <input type="text" placeholder="email" name="email" value={values.email} className="form-control w-75 d-block m-auto mt-4 text-center py-2" onChange={handleChange} onBlur={handleBlur} />
+          {errors.email && touched.email && <p className='error text-center'>{errors.email}</p>}
+
+          <input type="text" placeholder="phone-number" name="phoneNumber"  value={values.phoneNumber} onChange={handleChange} onBlur={handleBlur}className="form-control w-75 d-block m-auto mt-4 text-center py-2" />
+          {errors.phoneNumber && touched.phoneNumber && <p className='error text-center'>{errors.phoneNumber}</p>}
+
+          <input type="password" placeholder="password" name="password" className="form-control w-75 d-block m-auto mt-4 text-center py-2" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+          {errors.password && touched.password && <p className='error'>{errors.password}</p>}
+          
           <button className="btn btn-info px-3 py-2 w-75
           mt-5 fs-5 d-block m-auto border-0 userLogin text-white" onClick={registerUser}>Create Account</button>
             <p class='text-center' onClick={()=> navigate('/login')}>Already have an account? Login</p>
---
+++ src/Schemas/schema.js
@@ -0,0 +1,20 @@
+import * as Yup from 'yup'
+        
+const passwordRules = /^(?=.*\d)(?=.*[a-z]).{5,}$/;
+const numberRules = /^[\d]{11}$/
+
+export const basicSchema = Yup.object().shape({
+    email: Yup.string().email("Please enter a valid email").required("Required"),
+    username: Yup.string().min(2, "must be at least two characters").required("Required"),
+    lastname: Yup.string().min(2, "must be at least two characters").required("Required"),
+    phoneNumber: Yup
+        .string()
+        .matches(numberRules, "must be exactly 11 digits")
+        .max(11, "must not exceed 11 digits")
+        .required("Required"),
+    password: Yup
+        .string()
+        .min(6)
+        .matches(passwordRules, "must include a letter and a number")
+        .required("Required"),
+})
