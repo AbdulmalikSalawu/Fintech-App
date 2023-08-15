7d6d147c4454ca64bdf8eb9c13e994c041c52870
Salawu Abdulmalik <salawuabdulmalik90@gmail.com>
2023-08-14 21:21:12 -0700

    successfully displayed logged in user details on their dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -44,33 +44,9 @@ function Dashboard() {
           })
       }, [])
 
-      // const getDetails = () => {
-      //   fetch("https://abdulmalikyinka.onrender.com/userData", {
-      //     method: "POST",
-      //     crossDomain: true,
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Accept: "application/json",
-      //       "Access-Control-Allow-Origin": "*",
-      //     },
-      //     body: JSON.stringify({
-      //       token:localStorage.getItem("token"),
-      //     }),
-      //   })
-      //   .then((res)=>res.json())
-      //   .then((data)=> {
-      //       console.log(data, "userData")
-      //       setUserData(data.data)
-      //     })
-      //     .catch((error)=>{
-      //       console.log(error)
-      //     })
-      // }
-
       const logOut =()=> {
         localStorage.clear();
         navigate('/login')
-        // window.location.href = "/login"
       }
 
   return (
@@ -78,10 +54,8 @@ function Dashboard() {
       {showNav ? (
         <div>
           <h2 class='text-center'>Welcome to your dashboard!</h2>
-          <h1>{userData}</h1>
-          <h1>{userData.firstname}</h1>
-          {userData.email}
-            {/* <button onClick={getDetails} className="btn btn-info px-3 py-2 d-block m-auto mb-3 mt-5">Get details</button> */}
+          <h1 className='text-center text-success'>{userData.firstname}</h1>
+          <h1 className='text-center text-success'>{userData.email}</h1>
           <button onClick={logOut} class='btn btn-danger text-white px-3 py-2 mt-4 d-block m-auto'>Sign Out</button>
           </div>
       ) : ""}
