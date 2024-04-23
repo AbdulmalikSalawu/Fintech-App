3d53176217d7427434062799d41f3130f799d1cb
AbdulmalikSalawu <salawuabdulmalik90@gmail.com>
2024-04-23 14:22:37 +0100

    deleted users are now removed from the admin page

--- src/Components/Adminpage.js
+++ src/Components/Adminpage.js
@@ -4,15 +4,51 @@ function Adminpage() {
 
     const [data, setData] = useState([])
     useEffect(()=> {
-        fetch("https://abdulmalikyinka.onrender.com/allUsers", {
+        // fetch("https://abdulmalikyinka.onrender.com/allUsers", {
+            getAllUsers()
         // fetch("http://localhost:5000/allUsers", {
+        // method: "GET",})
+        // .then((res) => res.json())
+        // .then((data) => {
+        //     console.log(data, "allCustomers")
+        //     setData(data.data)
+        // })
+    }, [])
+
+    const getAllUsers = () => {
+        fetch("http://localhost:5000/allUsers", {
         method: "GET",})
         .then((res) => res.json())
         .then((data) => {
             console.log(data, "allCustomers")
             setData(data.data)
         })
-    }, [])
+    }
+
+    const deleteUser = (paramId,paramName) => {
+        if(window.confirm(`are you deleting ${paramName}`)){
+            // fetch("https://abdulmalikyinka.onrender.com/deleteUser", {
+                fetch("http://localhost:5000/deleteUser", {
+                  method: "POST",
+                  crossDomain: true,
+                  headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                  },
+                  body: JSON.stringify({
+                    uniqueid:paramId,
+                  }),
+                })
+                .then((res)=>res.json())
+                .then((data)=> {
+                    alert(data.data)
+                    getAllUsers()
+                  })
+        } else {
+
+        }
+    }
 
   return (
     <div>
@@ -21,14 +57,17 @@ function Adminpage() {
             <tr>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
-                <th scope="col">Phone Number</th>int
+                <th scope="col">Delete</th>
+                {/* <th scope="col">S/N</th> */}
             </tr>
             {data.map(i=>{
                 return(
-                    <tr>
-                        <td>{i.firstname}</td>
-                        <td>{i.email}</td>
-                        <td>{i.phoneNumber}</td>
+                    <tr key={i._id}>
+                        <td key={i.firstname}>{i.firstname}</td>
+                        <td key={i.email}>{i.email}</td>
+                        <td key={i._id} onClick={() => deleteUser(i._id,i.firstname)}>🗑</td>
+                        {/* <td>{i._id}</td> */}
+                        {/* <td>{i.phoneNumber}</td> */}
                     </tr>
                 )
             })}
